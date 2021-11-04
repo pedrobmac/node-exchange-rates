e6ab2a68b30e91bf940d5b32a2c1150c4daaedd6
Pedro Bittencourt <pbittenc@gmail.com>
2021-11-04 19:36:19 -0300

    implementing second source

--- api/controllers/RatesController.js
+++ api/controllers/RatesController.js
@@ -2,14 +2,17 @@ const { RatesServices } = require('../services')
 const ratesServices = new RatesServices()
 
 class RatesController {
+
   static async obtainRates(req, res) {
     const currencies = {
       from: req.query.from,
       to: req.query.to
     }
+
     try {
-      const data = await ratesServices.obtainFromFederalInstitute(currencies)
-      return res.status(200).json(data)
+      //const rates = await ratesServices.obtainFromFederalInstitute(currencies)
+      const rates = await ratesServices.obtainFromCentralBank()
+      return res.status(200).json(rates)
     } catch (error) {
       return res.status(500).json(error.message)
     }
--- api/services/RatesServices.js
+++ api/services/RatesServices.js
@@ -7,12 +7,19 @@ class RatesServices {
 
     async obtainFromFederalInstitute(currencies) {
         const address = `https://federal-institute.sandbox.swing.dev/rates/?base=${currencies.from}&target=${currencies.to}`
-        try {
-            const rates = await axios.get(address)
-            return rates.data
-        } catch (error) {
-            return error.message
-        }
+        const rates = await axios.get(address)
+        return rates.data
+    }
+
+    async obtainFromCentralBank() {
+        const address = "https://central-bank.sandbox.swing.dev/exchange/v1/"
+        const rates = await axios.get(
+            address, {
+            headers: {
+                'X-APIKEY': `SWING`
+            }
+        })
+        return rates.data
     }
 }
 
